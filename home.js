25946c73d41cce0689dd1722dc74acaeb5d25c31
Devipriya <devipriya.yedida@healthonus.io>
2023-11-15 17:46:33 +0530

    changes

--- home.js
+++ home.js
@@ -1,25 +1,66 @@
-//display dialog box
-function openDialog() {
-    var addItemDialog = document.getElementById('addItemDialog');
-    addItemDialog.showModal();
+function openDialog()
+{
+    document.getElementById('dialog').style.display = 'block';
+}
+function addContact() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var mobile = document.getElementById('mobile').value;
+   
+    var contact = {
+      name: name,
+      email: email,
+      mobile: mobile,
+    };
+   
+    saveContact(contact);
+    displayContact(contact);
+    closeDialog();
+  }
+   
+  function saveContact(contact) {
+    var existingContacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    existingContacts.push(contact);
+    localStorage.setItem('contacts', JSON.stringify(existingContacts));
+  }
+   
+  function displayContact(contact) {
+    var contactList = document.getElementById('contactList');
+    var contactElement = document.createElement('div');
+    contactElement.className = 'contact';
+    contactElement.innerHTML = `
+<strong>${contact.name}</strong><br>
+${contact.email}<br>
+      ${contact.mobile}<br><br>`;
+    contactList.appendChild(contactElement);
+  }
+   
+  function showFullDetails(contact) {
+    var fullDetailsBox = document.getElementById('fullDetailsBox');
+    fullDetailsBox.innerHTML = `
+Name: ${contact.name}<br>
+Email: ${contact.email}<br>
+      <strong>Mobile:</strong> ${contact.mobile}<br>
+      landline: ${contact.landline}<br>
+Website: ${contact.website}<br>
+      Address: ${contact.address}<br>;
+      `;
+fullDetailsBox.style.display = 'block';
+  }
+
+  
+   
+  function closeFullDetails() {
+    document.getElementById('fullDetailsBox').style.display = 'none';
+  }
+   
+  function closeDialog() {
+    document.getElementById('dialog').style.display = 'none';
   }
   
-  function closeDialog() {
-    var addItemDialog = document.getElementById('addItemDialog');
-    addItemDialog.close();
+   
+  function editContact(contact) {
   }
-
-//addthe form fields data
-function addItem() {
-  var form = document.getElementById('addItemForm');
-  // You can add your logic to handle the form data here
-  // For example, you can access form elements using form.elements
-  var name = form.elements['name'].value;
-  var email = form.elements['email'].value;
-  var phone = form.elements['phone'].value;
-
-  // Perform actions with the form data (e.g., add to a list, send to server, etc.)
-
-  // Close the dialog after handling the form data
-  closeDialog();
-}
+   
+  function deleteContact(contact) {
+  }
